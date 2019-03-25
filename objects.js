70244f9f2638b0d49f59e32f2761b98c60303d60
PS <palshankar@outlook.com>
2019-03-25 08:18:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,13 +7,3 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   delete(playlist.artistName);
 }
-  it('removes `artistName` from `playlist`', () => {
-    removeFromPlaylist(playlist, 'Slowdive')
-
-    expect(playlist).
-      to.contain.all.keys({'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-
-    expect(playlist).
-      not.to.have.all.keys({'Slowdive': 'Alison'})
-  })
-})
