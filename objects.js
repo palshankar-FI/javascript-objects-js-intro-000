8edb4b72a76e5d125662f4a477121539ec81bd5d
PS <palshankar@outlook.com>
2019-03-25 08:17:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,15 +3,5 @@ var playlist = { 'Slowdive' : "Alison", 'My Bloody Valentine' : "Sometimes"};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
 }
-  before(() => {
-    playlist['Slowdive'] = 'Alison'
-    playlist['My Bloody Valentine'] = 'Sometimes'
-  })
 
-  it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-    updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
-    expect(playlist).
-      to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
-  })
-})
