720bf591e10107566986c7395a9b55ba8795ac8c
PS <palshankar@outlook.com>
2019-03-25 08:19:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = { 'Slowdive' : "Alison", 'My Bloody Valentine' : "Sometimes"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
+  return(playlist);
 }
 
 function removeFromPlaylist(playlist, artistName) {
